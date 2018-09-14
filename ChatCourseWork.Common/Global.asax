0f2e9d68cb1ca6b6fb0d71fc1e57084fa9dabe13
Olga <olja225588@gmail.com>
2018-09-14 10:12:16 +0300

    Add project files.

---
+++ ChatCourseWork.Common/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatCourseWork.Common.MvcApplication" Language="C#" %>
---
+++ ChatCourseWork/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace ChatCourseWork.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return null;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return null;
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return null;
+        }
+    }
+}
